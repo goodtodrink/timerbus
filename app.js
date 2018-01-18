0d3641db1602758bfa244409b9e73aa58f828982
milkbala <justtaco0121@gmail.com>
2018-01-18 17:48:51 +0800

    make it better

--- app.js
+++ app.js
@@ -346,7 +346,7 @@ function GetAuthorizationHeader() {
     var HMAC = ShaObj.getHMAC('B64');
     var Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
 
-    return { 'Authorization': Authorization, 'X-Date': GMTString }; //如果要將js運行在伺服器，可額外加入 'Accept-Encoding': 'gzip'，要求壓縮以減少網路傳輸資料量
+    return { 'Authorization': Authorization, 'X-Date': GMTString };
 }
 
 app.listen(process.env.PORT || 3006);
