2c36b867c304291bcd452a29b1ae9f147ec62b29
milkbala <justtaco0121@gmail.com>
2018-01-18 16:35:00 +0800

    make it better

--- app.js
+++ app.js
@@ -228,13 +228,13 @@ app.get('/bus3_web',function(req,res){
             
             </head>
             <body>
-                // <script >
-                //     function myrefresh()
-                //      {
-                //         window.location.reload();
-                //      }
-                //     setTimeout('myrefresh()',1000);
-                //  </script>
+                <script >
+                    function myrefresh()
+                     {
+                        window.location.reload();
+                     }
+                    setTimeout('myrefresh()',30000);
+                 </script>
 
                     <script src="js/bootstrap.offcanvas.js"></script> 
                     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
